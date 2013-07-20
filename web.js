4234c427e046c941d6673d2a7a7598a0687cc366
Syed Tariq <syedtariq@ieee.org>
2013-07-20 15:00:17 +0000

    Initial

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  fs.readFile('index.html', 'ascii', function(err,data) {
+	if (err) throw err;
+  	response.send(data);
+	});
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
